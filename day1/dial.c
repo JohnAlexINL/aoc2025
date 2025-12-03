aa4a663226549834df8d76e23c2266e2a9994593
John Alex <bloodroserecords@gmail.com>
2025-12-02 18:09:40 -0600

    Day 1 Challenge complete

---
+++ day1/dial.c
@@ -0,0 +1,60 @@
+/*
+    The dial starts by pointing at 50.
+    The actual password is the number of times the dial
+    is left pointing at 0 after any rotation in the sequence.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "dat.h"
+
+#ifdef DEBUG
+#define log(...) printf(__VA_ARGS__)
+#else
+#define log(...) do {} while (0)
+#endif
+
+
+int dialPosition = 50;
+int dialZeros = 0;
+
+char * streamAdvance(char *input) {
+    while (*input && *input !='\n') { input++; } input++;
+    return input;
+}
+
+int dialTurn(int distance) {
+    dialPosition += distance;
+    while   ( dialPosition < 0 )        { dialPosition += 100; }
+    while   ( dialPosition >= 100 )     { dialPosition -= 100; }
+    if ( dialPosition == 0 ) { dialZeros ++; }
+    log(" { %2d } ", dialPosition);
+    return dialPosition;
+}
+
+int dialParse(char *input) {
+    int distance;
+    switch(*input) {
+        case 'L':   distance = -1; break;
+        case 'R':   distance =  1; break;
+        default:    log("ERR dialParse { char %d unexpected } %c%c%c%c ...\n", input[0], input[0], input[1], input[2], input[3]); exit(0); break;
+    }
+    distance *= atoi(input+1);
+    log("M%-4d  ", distance);
+    return distance;
+}
+
+int main () {
+    log("Processing data stream [%d bytes]\n", sizeof(data));
+    int steps = 0; char *stream = data; 
+    int cursor = 0;
+    while (1) {
+        cursor = (stream - data); if ( cursor >= sizeof(data)-1 ) { break; }
+        log("%-4d    ", cursor);
+        int distance = dialParse(stream);
+        int position = dialTurn(distance);
+        log("\n");
+        steps++; stream = streamAdvance(stream);
+    }
+    log("\nResult %d calculated in %d steps\n", dialZeros, steps);
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# [Advent of Code 2025](https://adventofcode.com/)
+
+This repo will contain my attempts at the AoC2025 challenges. In accordance with the AoC policies, the _inputs_ and _prompts_ will not be included in this repo, and to reproduce the outputs from the included source, you'll need to re-supply data from the challenges themselves.
+
+### Tooling
+
+I'll be doing this in C, 
+specifically C89/C99.
+I have access to the C Standard Library,
+a terminal, and an editor with syntax highlighting.
+
+### Additional Challenge
+
+I think once I solve a problem, I will go back through a second time and solve again with an added caveat; the second time has to be in a _language I don't know_. Those might include;
+
+- C# (`mcs` + `mono`)
+- Rust ()
+- Lisp
+- COBOL
+- Forth
+- Haskell
+
+And additionally, I am not allowed to use _tutorials_, instead I'll learn the syntax and general paradigm/flow of these languages by browsing GitHub for repos that use them.
